407fb955a98f43f2d5e10874a01516dfa19ac003
mishiyama <ayushlamsal931@gmail.com>
2025-10-03 18:19:33 +0545

    :)

--- 005.py
+++ 005.py
@@ -8,3 +8,22 @@ without any remainder.
 What is the smallest positive number that is evenly divisible by all of the numbers 
 from 1 to 20?
 """
+
+
+
+def gcd(a,b):
+ while b!=0:
+  c=a%b
+  a,b=b,c
+ return a
+
+
+def lcm(a,b):
+ return a*b /gcd(a,b)
+
+
+result=1
+for i in range(1,21):
+ result=lcm(result,i)
+
+print(result)
